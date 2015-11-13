12b3c0acfb42d1c083e7cfaf475ba2ffb34a03dc
Sumner Evans <sumner.evans98@gmail.com>
2015-11-12 20:51:57 -0700

    Initial Commit

---
+++ Pray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pray
+//
+//  Created by Sumner Evans on 2015/11/12.
+//  Copyright © 2015 SummationTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
